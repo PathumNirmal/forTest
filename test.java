4331df2dd55debd67c6aaa0386934c7af6a815a4
PathumNirmal <pathum.nirmal6@gmail.com>
2023-04-25 16:26:03 +0530

    initial commit

---
+++ test.java
@@ -0,0 +1,17 @@
+class Student{
+    String name;
+    int age;
+    String district;
+
+    public Student(String name, int age, String district){
+        this.name = name;
+        this.age = age;
+        this.district = district;
+    }
+}
+
+class Main{
+    public static void main(String args[]){
+        Student student1 = new Student("Pathum", 25, "Anuradhapura");
+    }
+}
